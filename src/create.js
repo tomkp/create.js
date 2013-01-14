e7f50ac64d18fefbdaab33bf57f7360b46b9d33c
tomkp <tom@tomkp.com>
2013-01-14 11:22:22 +0000

    docco tweaks

--- src/create.js
+++ src/create.js
@@ -1,15 +1,24 @@
 
 
-// Create
-//--------
+// ##Create
+//
 // An alternative to document.createElement
 //
-// Example:
+// ### Example:
 //
 //      var span = Create.element('span', 'Some text',
 //                      {id: 'abc', class: 'xyz'},
 //                      {click: function() {} }
 //                  );
+//
+// is just a shortcut for:
+//
+//      var element = document.createElement('span');
+//      element.appendChild(document.createTextNode('Some text'));
+//      element.setAttribute('id', 'abc');
+//      element.setAttribute('class', 'xyz');
+//      element.addEventListener('click', function() {});
+//
 //
 Create = (function () {
 
