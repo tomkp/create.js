0c572d46c1f9e262d7023577be4f9d326c569a12
tomkp <tom@tomkp.com>
2013-01-15 13:42:10 +0000

    Create elements using object data structure instead of parameters.  Recursively append children

--- src/create.js
+++ src/create.js
@@ -6,10 +6,12 @@
 //
 // ### Example:
 //
-//      var span = Create.element('span', 'Some text',
-//                      {id: 'abc', class: 'xyz'},
-//                      {click: function() {} }
-//                  );
+//      var span = Create.element({
+//                      tag: 'span',
+//                      contents: 'Some text',
+//                      attributes: {id: 'abc', class: 'xyz'},
+//                      events: {click: function() {} }
+//                 });
 //
 // is just a shortcut for:
 //
@@ -41,30 +43,36 @@ var Create = (function () {
                 var item = array[i];
                 // We always want to append elements - so that we don't
                 // lose any behaviours (event listeners etc)
-                if (typeof item == 'string') {
+                if (item.hasOwnProperty('tagName')) {
+                    // Assume that `item` is an element
+                    $element.appendChild(item);
+                }
+                else if (typeof item == 'string') {
                     // Create a text node for any strings
                     $element.appendChild(document.createTextNode(item));
                 } else {
-                    // Assume that `item` is an element
-                    $element.appendChild(item);
+                    // Assume it's an object representing more elements
+                    // Recursively append children
+                    $element.appendChild(element(item));
                 }
             }
         }
     };
 
+
     // Create an element with name `tag` (eg: span, li, img).
     // With `contents` [optional]: mixed element(s) or string(s).
     // And `attributes` [optional]: any attributes, (eg: id, class, name, title).
     // And `events` [optional]: any event listeners, (eg: click, mouseover).
-    var element = function (tag, contents, attributes, events) {
+    var element = function(options) {
         // Create the element
-        var $element = document.createElement(tag);
+        var $element = document.createElement(options['tag']);
         // Insert an array of html, elements into the element
-        insertContents($element, contents);
+        insertContents($element, options['contents']);
         // Add event listeners to it
-        augment($element, events, $element.addEventListener);
+        augment($element, options['events'], $element.addEventListener);
         // Set attributes on the element (eg: id, class, title)
-        augment($element, attributes, $element.setAttribute);
+        augment($element, options['attributes'], $element.setAttribute);
         return $element;
     };
 
--- test/tests/create-tests.js
+++ test/tests/create-tests.js
@@ -3,26 +3,26 @@ describe('Create Tests', function () {
 
 
     it('should create a span', function () {
-        var $element = Create.element('span');
+        var $element = Create.element({tag:'span'});
         expect($element.outerHTML).toEqual('<span></span>');
     });
 
 
     it('should create a span with inner html', function () {
-        var $element = Create.element('span', 'Some text');
+        var $element = Create.element({tag:'span', contents:'Some text'});
         expect($element.outerHTML).toEqual('<span>Some text</span>');
     });
 
 
     it('should create a div with an inner span and some text', function () {
         var arrayOfContent = [document.createElement('span'), 'Some text'];
-        var $element = Create.element('div', arrayOfContent);
+        var $element = Create.element({tag:'div', contents: arrayOfContent});
         expect($element.outerHTML).toEqual('<div><span></span>Some text</div>');
     });
 
 
     it('should create a span with inner html and attributes', function () {
-        var $element = Create.element('span', 'Some text', {class: 'xyz'});
+        var $element = Create.element({tag: 'span', contents: 'Some text', attributes: {class: 'xyz'}});
         expect($element.outerHTML).toEqual('<span class="xyz">Some text</span>');
     });
 
@@ -32,10 +32,38 @@ describe('Create Tests', function () {
             listener: function() {}
         };
         spyOn(spy, 'listener');
-        var $element = Create.element('button', null, null, {click: spy.listener});
+        var $element = Create.element({tag: 'button', events: {click: spy.listener}});
         $element.click();
         expect(spy.listener).toHaveBeenCalled();
     });
 
+
+    it('should allow for nesting elements', function () {
+        var $element = Create.element({
+            tag: 'ul',
+            attributes: {class: 'list'},
+            contents: [
+                Create.element({tag:'li', contents:'one'}),
+                Create.element({tag:'li', contents:'two'})
+            ]
+        });
+
+        expect($element.outerHTML).toEqual('<ul class="list"><li>one</li><li>two</li></ul>');
+    });
+
+
+    it('should allow for nested objects', function () {
+        var $element = Create.element({
+            tag: 'ul',
+            attributes: {class: 'list'},
+            contents: [
+                {tag:'li', contents:'one'},
+                {tag:'li', contents:'two'},
+                {tag: 'li', contents:'three'}
+            ]
+        });
+
+        expect($element.outerHTML).toEqual('<ul class="list"><li>one</li><li>two</li><li>three</li></ul>');
+    });
 
 });
